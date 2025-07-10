7346c821adf3709acfacb7d4f24fbb34eaf6a616
ktorres63 <ktorresaro@unsa.edu.pe>
2025-07-10 17:23:50 -0500

    Update astro.config

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_D9cYuafn.mjs';
-import { manifest } from './manifest_kE1wTO9p.mjs';
+import { manifest } from './manifest_DaUnlkIm.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -23,7 +23,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "9fd85767-89cd-4003-b3b0-dd376f0f5b90",
+    "middlewareSecret": "dc304d04-2285-410d-9a0a-188db3cce8e9",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
