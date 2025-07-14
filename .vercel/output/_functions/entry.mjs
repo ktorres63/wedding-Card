7045cc8a2decc87cedb77967cbff6eb0caf8ed11
ktorres63 <ktorresaro@unsa.edu.pe>
2025-07-14 13:52:11 -0500

    update .gitignore

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_CFAxugsK.mjs';
-import { manifest } from './manifest_Dxz06KSl.mjs';
+import { manifest } from './manifest_CXZcNdIB.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -21,7 +21,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "c28a090a-9169-41c0-98f7-c3de59a78aa7",
+    "middlewareSecret": "7a597ba6-dc27-40c5-9395-38ae5d682b5a",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
